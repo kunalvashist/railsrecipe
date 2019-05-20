9c6e0ffdb0232bbb4bbafab5d8382ebd3123eb8c
kunalvashist <kunal.vash@gmail.com>
2019-05-20 13:06:38 +0530

    added flash

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,31 @@ class RecipesController < ApplicationController
   def index
     @recipe = Recipe.all
   end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(gotoParam)
+    @recipe.chef = Chef.first
+    if @recipe.save
+      flash[:success] = " Recipe Created Successfully "
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def gotoParam
+    params.require(:recipe).permit(:name, :description)
+  end
+
 end
