915f6132a4ca03e2a322f12e4f51661f05bb2839
kunalvashist <kunal.vash@gmail.com>
2019-05-22 16:17:40 +0530

    Edit

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -11,6 +11,21 @@ class ChefsController < ApplicationController
     @chef = Chef.new
   end
 
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+
+  def update
+    @chef = Chef.find(params[:id])
+
+    if @chef.update(chef_params)
+      flash[:success] = "Updated Successfully"
+      redirect_to @chef
+    else
+      render "edit"
+    end
+  end
+
   def create
     @chef = Chef.new(chef_params)
     if @chef.save
---
+++ test/integration/chef_edit_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class ChefEditTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  #
+  def setup
+    @chef = Chef.create!(name:"kunal",email:"kunal@yopmail.com",
+                         password:"password",password_confirmation:"password")
+  end
+
+  test "Invalid Edit" do
+    get edit_chef_path(@chef)
+    assert_template "chefs/edit"
+    patch chefs_path(@chef), params: {chef: { name:" ",email:"kunal@yopmail.com"} }
+    assert_template "chefs/edit"
+    assert_select "h2.panel-title"
+    assert_select "div.panel-body"
+  end
+
+  test "Valid Edit" do
+    get edit_chef_path(@chef)
+    assert_template "chefs/edit"
+    patch chefs_path(@chef), params: {chef: { name:"kunal1",email:"kunal1@yopmail.com"} }
+    assert_redirect_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "kunal1", @chef.name
+    assert_match "kunal1@yopmail.com", @chef.email
+  end
+
+end
